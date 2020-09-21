d30995831fd36626f35280b8ca6d49734932fe24
Milan Gowda <46890041+MIlanJP@users.noreply.github.com>
2020-09-21 19:46:14 +0530

    update master branch

---
+++ src/components/ItemCard.jsx
@@ -0,0 +1,105 @@
+import { Button, Card, CardMedia } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+import React from "react";
+
+const useStyle = makeStyles((theme) => ({
+  mainLayout: {
+    // background:'black',
+    border: "black",
+    height: "315px",
+    width: "235px",
+  },
+  bookImage: {
+    height: "171px",
+    width: "233px",
+    paddingBottom: "2px",
+    background: "rgb(245,245,245)",
+  },
+  imageOfBook: {
+    marginTop: "7%",
+    height: "80%",
+  },
+  bookName: {
+      ...theme.typography.bookName,
+    paddingLeft: "25px",
+    whiteSpace: "nowrap",
+    textOverflow:'ellipsis',
+    textAlign: "left",
+    overflow:"hidden",
+    paddingRight: "20px",
+  },
+  authorName:{
+    ...theme.typography.authorName,
+      color:"#9D9D9D",
+      paddingLeft: "25px",
+      textAlign: "left",
+      lineHeight:".8rem",
+      position: "relative",
+      top: "-14px",
+      display:"flex",
+
+  },
+  priceColumn:{
+    ...theme.typography.priceColumn,
+      paddingLeft: "25px",
+      textAlign: "left",
+      top: "-21px",
+      position: "relative",
+  },
+  addToCartButton:{
+    ...theme.typography.buttonText,
+      background:"rgb(160,48,55)",
+      color:"white",
+    //   marginRight:"10px",
+      padding:"4px 10px 5px 10px",
+      "&:hover":{
+        ...theme.typography.buttonText,
+        background:"rgb(160,48,55)",
+        color:"white", 
+      }
+  },
+  buttonsColumn:{
+      position:'relative',
+      top:"-20px",
+      display:"flex",
+      flexDirection:"row",
+      justifyContent:"space-evenly",
+      
+  },
+  wishListButton:{
+    ...theme.typography.wishListButton,
+      boxShadow:" 0px 0px  3px 0px black",
+      padding:"3px 10px 3px 10px",
+
+    //   marginLeft:'10px'
+
+  },
+}));
+
+function ItemCard(props) {
+  const classes = useStyle();
+  return (
+    <Card className={classes.mainLayout}>
+      <div className={classes.bookImage}>
+        <img
+          className={classes.imageOfBook}
+          src={props.book.image}
+          alt={props.book.title}
+        />
+      </div>
+
+  <p className={classes.bookName} maxlength="20">{props.book.title}</p>
+  <span className={classes.authorName}> by {props.book.author}</span>
+  <p className={classes.priceColumn} >Rs {props.book.price}</p>
+     <div className={classes.buttonsColumn}  >
+         <Button className={classes.addToCartButton} 
+         onClick={()=>{console.log("jf")}}
+         >ADD TO BAG</Button>
+         <Button className={classes.wishListButton}>WISHLIST</Button>
+     </div>
+    </Card>
+  );
+}
+
+export default ItemCard;
---
+++ src/theme/theme.js
@@ -0,0 +1,55 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import yellow from '@material-ui/core/colors/yellow';
+import red from '@material-ui/core/colors/red';
+const fontForBookName="'Lato'"
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+        main:red[500]
+    },
+    secondary: {
+      main: yellow[500],
+    },
+    themeColor:{
+        
+    }
+  },
+  typography: {
+    bookName: {
+      fontFamily: fontForBookName,
+      fontWeight: 700,
+    //   fontSize: 10,
+      textTransform: "none",
+    },
+    authorName: {
+        fontFamily: fontForBookName,
+        // fontWeight: 700,
+        fontSize: 13.5,
+        textTransform: "none",
+      },
+      priceColumn:{
+        fontFamily: fontForBookName,
+        fontWeight: 700,
+        // fontSize: 13.5,
+        position: "relative",
+        top:'-10px',
+        textTransform: "none",  
+      },
+      buttonText:{
+        fontFamily: fontForBookName,
+        fontWeight: 300,
+        fontSize: 13,
+        textTransform: "none", 
+      },    
+        wishListButton:{
+        fontFamily: fontForBookName,
+        fontWeight: 700,
+        fontSize: 13.5,
+        textTransform: "none", 
+      },
+
+},
+});
+
+export default theme;
